551109273d4cacfff318295097b85cec3749b4c4
1Nathane <nathane.miranda@gmail.com>
2020-06-16 20:27:01 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,5 @@
 class Pessoa:
-    def __init__(self,*filhos, nome = None,idade = 35):
+    def __init__(self,*filhos, nome = None, idade = 50):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
@@ -8,12 +8,15 @@ class Pessoa:
         return f'Olá{id(self)}'
 
 if __name__ == '__main__':
-    nathane = Pessoa ( nome ='Nathane' )
-    luciano = Pessoa(nathane, nome = 'Luciano')
+    nathane = Pessoa(nome = 'Nathane', idade = 27)
+    layane = Pessoa(nome = 'Layane', idade = 27)
+    rafael = Pessoa(nome = 'Rafael', idade = 18)
+    luciano = Pessoa(nathane,rafael,layane, nome = 'Luciano')
     print(Pessoa.cumprimentar(luciano))
-    print( id( luciano ) )
-    print( luciano.cumprimentar() )
-    print( luciano.nome)
-    print(luciano.idade)
-    for filho in luciano.filhos:
+    print(id(luciano))
+    print(luciano.cumprimentar())#Mesma expressão da linha 15
+    print(f'Nome: {luciano.nome}')
+    print(f'Idade: {luciano.idade}')
+    print(f'Filhos de {luciano.nome}:')
+    for filho in luciano.filhos:#Necessário porque os filhos de luciano são objetos da classe Pessoa
         print (filho.nome)
